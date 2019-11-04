e0ac35175f48e62a78972c29fc3c27e85fa93753
Sarah Wood <wood.sarah@outlook.com>
2019-11-04 18:21:07 +0000

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,4 @@
 (function () {
-  const gameElem = document.getElementById('game')
 
   const CELL_EMPTY = 0
   const CELL_SNAKE = 1
@@ -18,38 +17,92 @@
 
   }
 
-  function updateScore(scoreElem, gameState) {
-
-  }
-
   function updateSnake(gameState) {
 
   }
 
-  function setupGame(width, height, speed) {
+  function createGrid(width, height, game) {
+    const grid = document.createElement('div')
+    grid.className = 'grid'
+    game.appendChild(grid)
 
+    const cells = []
+    for (let x = 0; x < width; x++) {
+      // Make an array for Y
+      if (cells[x] === undefined) {
+        cells[x] = []
+      }
+      // Create row element
+      for (let y = 0; y < width; y++) {
+        // Ceate cell element
+        // Append cell to row
+      }
+      // Append row to grid
+    }
+
+    function remove() {
+      game.removeChild(grid)
+    }
+
+    return {
+      remove
+    }
   }
 
-  function createGrid(width, height, cellSize) {
+  function createScore(initialScore, game) {
+    const scoreDiv = document.createElement('div')
+    scoreDiv.className = 'score'
+    game.appendChild(scoreDiv)
 
+    const score = {
+      value: initialScore,
+      update,
+      remove
+    }
+
+    function update(newValue) {
+      score.value = newValue
+      scoreDiv.textContent = newValue
+    }
+
+    function remove() {
+      game.removeChild(scoreDiv)
+    }
+
+    update()
+    return score
   }
 
-  function setupGame(width, height, cellSize, speed) {
+  function setupGame(gameSelector, width, height, speed) {
+    const game = document.querySelector(gameSelector)
     let gameState
-    let scoreElem
     let updateTimeout
     const startX = parseInt(width / 2, 10)
     const startY = parseInt(height / 2, 10)
 
     function onKeyDown(event) {
-
+      switch (event.key) {
+        case 'ArrowUp':
+          gameState.direction = DIR_UP
+          return
+        case 'ArrowRight':
+          gameState.direction = DIR_RIGHT
+          return
+        case 'ArrowDown':
+          gameState.direction = DIR_DOWN
+          return
+        case 'ArrowLeft':
+          gameState.direction = DIR_LEFT
+          return
+      }
     }
 
     function start() {
-      const grid = createGrid(width, height, cellSize)
+      const grid = createGrid(width, height, game)
+      const score = createScore(0, game)
       const snake = [{ x: startX, y: startY }]
       gameState = {
-        score: 0,
+        score,
         grid,
         direction: DIR_UP,
         snake
@@ -59,13 +112,15 @@
     }
 
     function update() {
-      console.log('update')
+      console.log(gameState)
       updateTimeout = setTimeout(update, speed)
     }
 
     function end() {
       gameState = undefined
       document.removeEventListener('keydown', onKeyDown)
+      gameState.grid.remove()
+      gameState.score.remove()
     }
 
     function restart() {
@@ -76,6 +131,7 @@
       clearInterval(updateTimeout)
     }
 
+
     return {
       start,
       end,
@@ -85,7 +141,9 @@
 
   }
 
-  setupGame(11, 11, 20, 100).start()
+  document.addEventListener('DOMContentLoaded', () => {
+    setupGame('#game', 11, 11, 20, 1000).start()
+  })
 
 })()
 
